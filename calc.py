140d037619a9a5ff0a337432f72496b76064dba5
abhibth977 <abhibth977@gmail.com>
2024-09-30 00:46:10 +0530

    updating

--- calc.py
+++ calc.py
@@ -14,10 +14,24 @@ def cpga_calculator():
     total_credits = total_grade_points = 0
     grade_points_dict = {"A+":10, "A":9, "B+":8, "B":7, "C":6, "D":5, "F":0}
 
-    labels = ["Course Name", "Credit", "Grade"]
-    for i, text in enumerate(labels):
-        ttk.Label(frame, text=text, background="yellow", font=("Arial", 12, "bold"))
-        Label.grid(row=0, column=i, padx=10, pady=5, sticky="ew")
+    for course_name, course_credit, grade_list in course_rows:
+        try:
+            credits = float(course_credit.get())
+            grade = grade_menu.get()
+            
+            if grade not in grade_points_dict:
+                raise ValueError("Invalid grade selected")
+            
+            total_credits += credits
+            total_grade_points += credits * grade_points_dict[grade]
+            
+    except ValueError as ve:
+        messagebox.showerror("Error", f"Invalid input:{ve}")
+        return
+    
+    if total_credits == 0:
+        messagebox.showerror("Error", "Total credits cannot be zero")
+        return
     
     cgpa = total_grade_points / total_credits
     result.config(text=f"CGPA: {cgpa:.2f}")
