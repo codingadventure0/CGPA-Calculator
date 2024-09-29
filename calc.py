cf86a6c77d3de035eb36381d8d98951cc7288ac5
abhibth977 <abhibth977@gmail.com>
2024-09-30 00:34:21 +0530

    updating

--- calc.py
+++ calc.py
@@ -13,6 +13,11 @@ course_rows = []
 def cpga_calculator():
     total_credits = total_grade_points = 0
     grade_points_dict = {"A+":10, "A":9, "B+":8, "B":7, "C":6, "D":5, "F":0}
+
+    labels = ["Course Name", "Credit", "Grade"]
+    for i, text in enumerate(labels):
+        ttk.Label(frame, text=text, background="yellow", font=("Arial", 12, "bold"))
+        Label.grid(row=0, column=i, padx=10, pady=5, sticky="ew")
     
     cgpa = total_grade_points / total_credits
     result.config(text=f"CGPA: {cgpa:.2f}")
